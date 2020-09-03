d26a9c2219157cd6268bb044b7ca715f6f48e3cc
vinicius <viniciusssmartins@gmail.com>
2020-09-03 20:33:14 -0300

    branding new cute buttons

--- js/app.js
+++ js/app.js
@@ -12,3 +12,90 @@ function nextImage() {
   currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
   slideshowImages[currentImageCounter].style.opacity = 1;
 }
+
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const openModalButtons2 = document.querySelectorAll('[data-modal-target2]')
+const closeModalButtons2 = document.querySelectorAll('[data-close-button2]')
+const openModalButtons3 = document.querySelectorAll('[data-modal-target3]')
+const closeModalButtons3 = document.querySelectorAll('[data-close-button3]')
+const openModalButtons4 = document.querySelectorAll('[data-modal-target4]')
+const closeModalButtons4 = document.querySelectorAll('[data-close-button4]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+openModalButtons2.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal2)
+  })
+})
+
+openModalButtons3.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal3)
+  })
+})
+
+openModalButtons4.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal4)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  moals.forEach(modal => {
+    closeModal(modal)
+  })
+
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+closeModalButtons2.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal2)
+  })
+})
+
+closeModalButtons3.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal3)
+  })
+})
+
+closeModalButtons4.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal4)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
